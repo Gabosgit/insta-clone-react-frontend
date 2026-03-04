27ec4a1c01b86811ee523e0373534ff1daaeeafa
Gabo <no@email>
2026-03-04 23:16:38 +0100

    feat(CreateMenu): restructure layout for improved user guidance and interaction

--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -2,17 +2,23 @@ import { CreateCard } from "~/components/CreateCard";
 
 export default function CreateMenu() {
   return (
-    <div className="grid grid-cols-2 w-full h-full gap-4 place-items-center">
-        <CreateCard 
-          itemType="Post" 
-          to="/profile/create/post"
-          text="Add a photo/video and text to create a post."
-        />
-        <CreateCard 
-          itemType="Reel" 
-          to="/profile/create/reel"
-          text="Add a video to create a reel."
-        />
+    <div className="flex flex-col h-full items-center justify-center">
+        <h1 className="text-3xl font-bold mb-8">
+          What would you like to create?
+        </h1>
+        <div className="grid grid-cols-2 w-full gap-4 place-items-center">
+          <CreateCard 
+            itemType="Post" 
+            to="/profile/create/post"
+            text="Add a photo/video and text to create a post."
+          />
+          <CreateCard 
+            itemType="Reel" 
+            to="/profile/create/reel"
+            text="Add a video to create a reel."
+          />
+        </div>
     </div>
+    
   );
 }
